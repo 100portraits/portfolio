a26d61ba95cb10879bf1cbf674741f36008b541e
sahir <sahirde@gmail.com>
2023-09-28 15:43:07 +0200

    add icons, description, formatting

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,10 +1,25 @@
+import {FaGithub} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+
 const Home = () => {
     return (
         <>
         
-            <div className=" max-w-screen-lg mx-auto">
-                <div className="text-left mt-14 mb-4">
-                    <h1 className="text-5xl font-light">Sahir Dhanani Enarth</h1>
+            <div className=" max-w-screen-lg p-10 mx-auto">
+                <div className="text-left mt-14 mb-4 flex justify-between">
+                    <div className="">
+                        <h1 className="pb-1 text-6xl font-semibold">Sahir Dhanani Enarth</h1>
+                        <h1 className=" text-2xl ">Web design / photography / data science</h1>
+                        <h1 className="text-xl">Computational Social Science @ University of Amsterdam</h1>
+                    </div>
+                    <div className='self-end flex flex-row gap-2 text-3xl'>
+                        <FaGithub />
+                        <FaInstagram/>
+
+                    </div>
+                </div>
+                <div>
+                    <p className="text-3xl font-medium pb-4">My work</p>
                 </div>
                 <div className='grid grid-cols-3 gap-4 h-[80vh]'>
                     <div className='bg-slate-100 h-full col-span-2'></div>
