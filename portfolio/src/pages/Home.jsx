f2688dd1f4f8c833b958e94a6d3ffc4db55cdbb6
sahir <sahirde@gmail.com>
2023-09-28 15:06:24 +0200

    boilerplate

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+const Home = () => {
+    return (
+        <>
+        
+            <div className=" max-w-screen-lg mx-auto">
+                <div className="text-left mt-14 mb-4">
+                    <h1 className="text-5xl font-light">Sahir Dhanani Enarth</h1>
+                </div>
+                <div className='grid grid-cols-3 gap-4 h-[80vh]'>
+                    <div className='bg-slate-100 h-full col-span-2'></div>
+                    <div className='bg-slate-100 h-full'></div>
+                    <div className='bg-slate-100 h-full'></div>
+                    <div className='bg-slate-100 h-full col-span-2'></div>
+
+                </div>
+            
+
+            </div>
+        </>
+    );
+}
+
+export default Home;
