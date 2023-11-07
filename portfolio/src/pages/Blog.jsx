7837a96f29edfa2d816a2132b83ba1f24021daec
sahir <sahirde@gmail.com>
2023-11-07 18:56:31 +0100

    blog titles, no content hahahaha

--- portfolio/src/pages/Blog.jsx
+++ portfolio/src/pages/Blog.jsx
@@ -1,8 +1,37 @@
+import Template from "./Template"
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 const Blog = () => {
     return (
-        <div>
-            Blog
-        </div>
+        <Template
+            backarrow={true}
+            header='Projects'
+            subheader=''
+            subheader2="The making of things that I made! Hopefully helpful."
+            icons={
+                <>
+                    <FaGithub/>
+                    <FaInstagram/>
+                </>
+            }
+            contentlist={[
+                {
+                    title: 'Unpacking Clusters from BERTopic models',
+                    link: '/'
+                },
+                {
+                    title: 'Making this website',
+                    link: '/'
+                },
+                {
+                    title: 'Making 100portraits',
+                    link: '/'
+                },
+            
+                
+            ]}
+            
+        />
     );
 }
 
