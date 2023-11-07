520ab5fbd54b3730c3be611fcb994d3943c0276c
sahir <sahirde@gmail.com>
2023-11-07 17:42:28 +0100

    cheating

--- portfolio/src/components/darkModeSwitch.jsx
+++ portfolio/src/components/darkModeSwitch.jsx
@@ -1,30 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from "react";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
+import useDarkSide from "../hooks/useDarkSize";
+export default function ThemeSwitcher() {
 
-const ThemeSwitcher = () => {
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const isDarkMode = localStorage.getItem('darkMode') === 'true';
-    setDarkMode(isDarkMode);
-  }, []);
-
-  useEffect(() => {
-    document.documentElement.classList.toggle('dark', darkMode);
-    localStorage.setItem('darkMode', darkMode);
-  }, [darkMode]);
-
-  const toggleDarkMode = () => {
-    setDarkMode((prevMode) => !prevMode);
-  };
-
-  return (
-    <button
-      onClick={toggleDarkMode}
-      className="px-4 py-2 rounded-md bg-slate-700 text-white"
-    >
-      {darkMode ? 'Light Mode' : 'Dark Mode'}
-    </button>
+  const [colorTheme, setTheme] = useDarkSide();
+    const [darkSide, setDarkSide] = useState(
+        colorTheme === "light" ? true : false
+    );
+ 
+    const toggleDarkMode = (checked) => {
+        setTheme(colorTheme);
+        setDarkSide(checked);
+    };
+    return (
+      <>
+          <DarkModeSwitch
+              style={{ marginBottom: "2rem" }}
+              checked={darkSide}
+              onChange={toggleDarkMode}
+              size={30}
+          />
+      </>
   );
 };
-
-export default ThemeSwitcher;
